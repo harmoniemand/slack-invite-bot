8eee5fa0803a5acb971a44b7334145e5d4702176
harmoniemand <jonathan.guenz@outlook.com>
2016-10-26 00:10:01 +0200

    foobar

--- app.js
+++ app.js
@@ -1,25 +1,66 @@
 const config = require("./config");
 
-const credentials = {
-    client: {
-        id: config.client_id,
-        secret: config.client_secret
-    },
-    auth: {
-        tokenHost: "https://slack.com/api/oauth.access"
+'use strict';
+
+const express = require('express');
+const simpleOauthModule = require('./../');
+
+const app = express();
+const oauth2 = simpleOauthModule.create({
+  client: {
+    id: '<CLIENT_ID>',
+    secret: '<CLIENT_SECRET>',
+  },
+  auth: {
+    tokenHost: 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize',
+  },
+});
+
+// Authorization uri definition
+const authorizationUri = oauth2.authorizationCode.authorizeURL({
+  redirect_uri: 'http://localhost:3000/callback',
+  scope: 'notifications',
+  state: '3(#0/!~',
+});
+
+// Initial page redirecting to Github
+app.get('/auth', (req, res) => {
+  console.log(authorizationUri);
+  res.redirect(authorizationUri);
+});
+
+// Callback service parsing the authorization token and asking for the access token
+app.get('/callback', (req, res) => {
+  const code = req.query.code;
+  const options = {
+    code,
+  };
+
+  oauth2.authorizationCode.getToken(options, (error, result) => {
+    if (error) {
+      console.error('Access Token Error', error.message);
+      return res.json('Authentication failed');
     }
-};
 
-const oauth2 = require('simple-oauth2').create(credentials);
+    console.log('The resulting token: ', result);
+    const token = oauth2.accessToken.create(result);
 
-const tokenConfig = {
-  code: config.state
-};
+    return res
+      .status(200)
+      .json(token);
+  });
+});
 
-oauth2.authorizationCode.getToken(tokenConfig, (error, result) => {
-  if (error) {
-    return console.log('Access Token Error', error.message);
-  }
+app.get('/success', (req, res) => {
+  res.send('');
+});
 
-  const token = oauth2.accessToken.create(result);
+app.get('/', (req, res) => {
+  res.send('Hello<br><a href="/auth">Log in with Github</a>');
+});
+
+app.listen(3000, () => {
+  console.log('Express server started on port 3000'); // eslint-disable-line
 });
